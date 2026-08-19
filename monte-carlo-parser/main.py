e3923ca944a589a71ac0c607247c36d8546395b7
Fahmi Rafif Tiansyah <tiansyahrocket@gmail.com>
2026-08-19 09:54:08 +0700

    message: Improvements

--- monte-carlo-parser/main.py
+++ monte-carlo-parser/main.py
@@ -25,7 +25,7 @@ app.mount('/static', StaticFiles(directory='data/processed'), name='static')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
